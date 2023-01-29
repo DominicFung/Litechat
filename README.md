dbb813ebd2a7a58f6c84b3feee864f7471cc10ff
Dom Fung <fung_dominic@hotmail.com>
2023-01-29 17:07:16 -0500

    chat scaffold, test package added

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# LiteChat
+# `@domfung/litechat`
 
-Free React Chat that hooks up to Discord Bot, so you can chat with your customers from discord.
+> This `@domfung/litechat` is an open source react/tailwind chat you can integrate into your frontend that hooks up to discord as a chatbot. Now you can chat directly with customers on your website directly on your discord channel! 
+
+> Dom owns the shared backend (FREE for now!), however an example is provided for AWS (cdk, serverless) if you wish to deploy your own.
+
+[![npm version](https://badge.fury.io/js/%40apollo%2Fserver.svg)](https://badge.fury.io/js/%40apollo%2Fserver)
+[![Build Status](https://circleci.com/gh/apollographql/apollo-server.svg?style=svg)](https://circleci.com/gh/apollographql/apollo-server)
+[![Join the community](https://img.shields.io/discourse/status?label=Join%20the%20community&server=https%3A%2F%2Fcommunity.apollographql.com)](https://community.apollographql.com)
+[![Join our Discord server](https://img.shields.io/discord/1022972389463687228.svg?color=7389D8&labelColor=6A7EC2&logo=discord&logoColor=ffffff&style=flat-square)](https://discord.gg/graphos)
+
+## A Simple Chat Component for your Frontend
+
+```bash
+npm i @domfung/litechat
+```
+
+to use:
+
+```typescript
+import { LiteChat } from '@domfung/litechat'
+
+...
+
+return (
+  ...
+    <LiteChat storeId="<YOUR STORE ID>" enableCookie={true} />
+  </>
+)
+
+```
+
+## Advanced Usage
+
+To customize the storage of
--- src/chat.tsx
+++ src/chat.tsx
@@ -1,75 +1,132 @@
-// import { useEffect, useState } from "react"
-// import { Client, GatewayIntentBits, TextBasedChannel } from 'discord.js'
-// import React from "react"
-
-// interface LiteChatProps {
-//   secret: string,
-//   channelId: string
-// }
-
-// export function LiteChat(props: LiteChatProps) {
-//   const [client, setClient] = useState<Client<boolean>>()
-
-//   const init = async () => {
-//     const client = new Client({
-//       intents: [
-//         GatewayIntentBits.Guilds,
-//         GatewayIntentBits.GuildMessages,
-//         GatewayIntentBits.MessageContent
-//       ],
-//     })
-  
-//     client.login(props.secret)
-  
-//     console.log(client)
-//     await new Promise<void>((resolve, reject) => {
-//       client.once("error", reject)
-//       client.once("ready", () => {
-//         client.off("error", reject)
-//         resolve()
-//       })
-//     })
-  
-//     console.log(client.isReady())
-//     setClient(client)
-//   }
-
-//   const postDiscord = async () => {
-//     console.log("post to discord")
-//     if (client) {
-//       const channel = client.channels.cache.get(props.channelId) as TextBasedChannel
-//       console.log(channel)
-    
-//       if (channel) {
-//         console.log(`channelId: ${channel.id}, type: ${channel.type}`)
-    
-//         let r = await channel.send("/imagine beautiful landing for puppy portrait website, ux/ui, ux, ui, cute, pink and blue --v 4 --ar 3:2 --q 2")
-//         console.log(r)
-//         console.log(" ==== DONE ==== ")
-    
-    
-//       } else console.error("No Discord Channel Found.")
-//     }
-//   }
-
-//   useEffect(() => { init() }, [])
-
-//   return (
-//     <>
-//       <p>Ready? {client ? "yes" : "no"}</p>
-//       <button onClick={postDiscord}>Click me</button>
-//     </>
-//   );
-// }
-
 import * as React from "react";
+import './index.css'
 
 export function LiteChat() {
-  const [count, setCount] = React.useState(0);
+  const [ open, setOpen ] = React.useState(false)
+
+
   return (
     <>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <div className="fixed bottom-5 right-5">
+        <button onClick={() => { setOpen(true) }}
+          className="p-0 w-16 h-16 bg-blue-600 rounded-full hover:bg-blue-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none text-white">
+          {/* <svg viewBox="0 0 20 20" enable-background="new 0 0 20 20" className="w-6 h-6 inline-block">
+            <path fill="#FFFFFF" d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
+                                    C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
+                                    C15.952,9,16,9.447,16,10z" />
+          </svg> */}
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 inline-block">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+          </svg>
+        </button>
+      </div>
+
+      {open && <div className={`fixed bottom-0 right-10 w-80`}>
+        <div className="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-t-lg overflow-hidden h-92">
+          <div className="flex flex-row bg-blue-500">
+            <div className="flex-grow" />
+            <button className="p-1 text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6" />
+              </svg>
+            </button>
+            <button className="p-1 text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+          
+          <div className="flex flex-col flex-grow h-0 p-4 overflow-auto">
+            <div className="flex w-full mt-2 space-x-3 max-w-xs">
+              <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+              <div>
+                <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                  <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+              </div>
+            </div>
+            <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+              <div>
+                <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                  <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+                </div>
+                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+              </div>
+              <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+            </div>
+            <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+              <div>
+                <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                  <p className="text-sm">Lorem ipsum dolor sit amet.</p>
+                </div>
+                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+              </div>
+              <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+            </div>
+            <div className="flex w-full mt-2 space-x-3 max-w-xs">
+              <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+              <div>
+                <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                  <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                </div>
+                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+              </div>
+            </div>
+            <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+              <div>
+                <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                  <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                </div>
+                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+              </div>
+              <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+            </div>
+            <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+              <div>
+                <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                  <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                </div>
+                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+              </div>
+              <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+            </div>
+            <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+              <div>
+                <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                  <p className="text-sm">Lorem ipsum dolor sit amet.</p>
+                </div>
+                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+              </div>
+              <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+            </div>
+            <div className="flex w-full mt-2 space-x-3 max-w-xs">
+              <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+              <div>
+                <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                  <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                </div>
+                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+              </div>
+            </div>
+            <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+              <div>
+                <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                  <p className="text-sm">Lorem ipsum dolor sit.</p>
+                </div>
+                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+              </div>
+              <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+            </div>
+          </div>
+          
+          <div className="bg-blue-300 p-4">
+            <input className="flex items-center h-10 w-full rounded px-3 text-sm" type="text" placeholder="Type your message…" />
+          </div>
+        </div>
+
+      </div>}
     </>
   );
 }
